d6aecffb897273c8e223830e85cda5a0840c6ddd
hamzag <hamza@skypher.co>
2023-12-05 20:57:55 +0100

    Change duplicates

--- airflow/dags/dag_kafka_spark.py
+++ airflow/dags/dag_kafka_spark.py
@@ -6,14 +6,6 @@ from datetime import datetime
 
 from src.kafka_client.kafka_stream_data import stream
 
-from datetime import timedelta
-from airflow import DAG
-from airflow.operators.python import PythonOperator
-from airflow.providers.docker.operators.docker import DockerOperator
-from datetime import datetime
-
-from src.kafka_client.kafka_stream_data import stream
-
 
 start_date = datetime.today() - timedelta(days=1)
 
@@ -28,44 +20,6 @@ default_args = {
 
 with DAG(
     dag_id="kafka_spark_dag",
-    default_args=default_args,
-    schedule_interval=timedelta(days=1),
-    catchup=False,
-) as dag:
-
-    kafka_stream_task = PythonOperator(
-        task_id="kafka_data_stream",
-        python_callable=stream,
-        dag=dag,
-    )
-
-    spark_stream_task = DockerOperator(
-        task_id="pyspark_consumer",
-        image="rappel-conso/spark:latest",
-        api_version="auto",
-        auto_remove=True,
-        command="./bin/spark-submit --master local[*] --packages org.postgresql:postgresql:42.5.4,org.apache.spark:spark-sql-kafka-0-10_2.12:3.5.0 ./spark_streaming.py",
-        docker_url='tcp://docker-proxy:2375',
-        environment={'SPARK_LOCAL_HOSTNAME': 'localhost'},
-        network_mode="airflow-kafka",
-        dag=dag,
-    )
-
-
-    kafka_stream_task >> spark_stream_task
-start_date = datetime.today() - timedelta(days=1)
-
-
-default_args = {
-    "owner": "airflow",
-    "start_date": start_date,
-    "retries": 1,  # number of retries before failing the task
-    "retry_delay": timedelta(seconds=5),
-}
-
-
-with DAG(
-    dag_id="kafka_spark_DAG",
     default_args=default_args,
     schedule_interval=timedelta(days=1),
     catchup=False,
